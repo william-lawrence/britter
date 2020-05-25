1044cbb77c6f390da47a4357ae9cad2bf0f9c447
William <williamjlawrence1@gmail.com>
2020-05-25 15:33:08 -0400

    added some auth

--- app/controllers/quips_controller.rb
+++ app/controllers/quips_controller.rb
@@ -1,6 +1,7 @@
 class QuipsController < ApplicationController
   before_action :set_quip, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  
   # GET /quips
   # GET /quips.json
   def index
