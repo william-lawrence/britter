7f062c6a6f7afdff790f446d20ac9697c1ce7e64
William Lawrence <williamlawrence@bravowell.com>
2020-05-28 10:42:56 -0400

    Added descriptions, started adding profile views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users , :controllers => {registrations: 'registrations'}
   devise_for :models
   resources :quips
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_195639) do
+ActiveRecord::Schema.define(version: 2020_05_28_142424) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_195639) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "username"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
